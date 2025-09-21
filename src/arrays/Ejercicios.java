68933358cc4ba4fc528f2039d9bc1f8a306ff019
octavio sosa <121409253+ottisosa@users.noreply.github.com>
2025-09-20 22:21:50 -0300

    Add files via upload

--- src/arrays/Ejercicios.java
+++ src/arrays/Ejercicios.java
@@ -9,28 +9,47 @@ public class Ejercicios {
 
         Scanner sc = new Scanner(System.in);
 
+        
+        //  PRIMER ARRAY  //
+        
         System.out.print("Ingrese el largo del array: ");
 
         int cantidad = sc.nextInt();
 
-        int[] datos = new int[cantidad];
+        int[] datos1 = new int[cantidad];
 
-        for (int i = 0; i < datos.length; i++) {
+        for (int i = 0; i < datos1.length; i++) {
 
             System.out.print("Ingrese valor para la poscicion " + i + ": ");
-            datos[i] = sc.nextInt();
+            datos1[i] = sc.nextInt();
         }
+        
+        
+        //  SEGUNDO ARRAY  //
+        
+        System.out.println("Ingrese longitud del segundo array");
 
-        System.out.println(Arrays.toString(datos));
+        int cantidad2 = sc.nextInt();
+        
+        int[] datos2 = new int [cantidad2];
+        
+        for (int i = 0; i < datos2.length; i++) {
+            
+            System.out.println("Ingrese el valor para la poscicion " + i + ": ");
+            datos2[i] = sc.nextInt();
+        }
+        
+        System.out.println(Arrays.toString(datos1));
 
-//        Ejercicio1(datos);
-//        Ejercicio2(datos);
-        Ejercicio3(datos);
+//        Ejercicio1(datos1);
+//        Ejercicio2(datos1);
+//        Ejercicio3(datos1);
+//        Ejercicio5(datos1,datos2);
 
     }
 
     // EJERCICIO 1 //
-    public static void Ejercicio1(int[] datos) {
+    public static void Ejercicio1(int[] datos1) {
 
         char elem = '*';
         for (int i = 0; i < datos.length; i++) {
@@ -49,7 +68,7 @@ public class Ejercicios {
     }
 
     // EJERCICIO 2 //
-    public static void Ejercicio2(int[] datos) {
+    public static void Ejercicio2(int[] datos1) {
 
         for (int i = datos.length - 1; i >= 0; i--) {
 
@@ -60,7 +79,7 @@ public class Ejercicios {
     }
 
     // EJERCICIO 3 //
-    public static void Ejercicio3(int[] datos) {
+    public static void Ejercicio3(int[] datos1) {
 
         boolean ascendente = true;
         boolean descendente = true;
@@ -102,4 +121,23 @@ public class Ejercicios {
 
     }
 
+    // EJERCICIO 5 //
+    
+    public static void Ejercicio5(int[] datos1 , int[] datos2){
+    
+        int [] respuesta;
+        
+        for (int i = 0; i < datos1.length; i++) {
+         
+            for (int j = 0; j < datos2.length; j++) {
+              
+                if(datos2[i] < datos1[i]){
+               
+                }
+                
+            }
+            
+        }
+        
+    }
 }
